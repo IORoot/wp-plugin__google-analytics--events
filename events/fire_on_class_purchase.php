bfa109616cb36fa0da5e0311434666393f44eb46
Andy Pearson <andy@londonparkour.com>
2020-01-25 08:23:38 +0000

    change action

--- events/fire_on_class_purchase.php
+++ events/fire_on_class_purchase.php
@@ -9,9 +9,35 @@
 //  └─────────────────────────────────────────────────────────────────────────┘░
 //   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-add_action('tbk_reservation_email_to_customer', 'analytics_goal_hit_class_booking', 10, 2);
+// add_action('tbk_reservation_email_to_customer', 'analytics_goal_hit_class_booking', 10, 2);
 
-function analytics_goal_hit_class_booking($email, $reservation){
+// function analytics_goal_hit_class_booking($email, $reservation){
+
+//         // create new object
+//         $GA = new send_analytics();
+
+//         /**
+//          * Fire an analytics event
+//          * 
+//          * Category = 'class booking'
+//          * Action = 'Beginner Class'
+//          * Label = 'A Person'
+//          * Value = 10
+//          */
+//         $GA->send_analytics_event(
+//             'class booking',
+//             $reservation->getServiceName(),
+//             $reservation->getCustomerDisplayName(),
+//             $reservation->getPrice()
+//         );
+
+//     return;
+// }
+
+
+add_action('tbk_reservation_completed', 'analytics_goal_hit_class_booking', 10, 2);
+
+function analytics_goal_hit_class_booking($reservation){
 
         // create new object
         $GA = new send_analytics();
